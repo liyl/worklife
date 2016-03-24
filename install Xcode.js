60c1abcad9a766b982ab36f43c6f12dd9da8c337
liyl <liyl_tao@163.com>
2016-03-24 14:37:32 +0800

    how to install xcode in china

---
+++ install Xcode.js
@@ -0,0 +1,8 @@
+How to install Xcode ?  
+1.AppStore ? Oh No! No! No!
+2.On webSite ? May be is right
+
+	first：login in Apple Developr webSite  (https://developer.apple.com/)
+	second : go to this page : (https://developer.apple.com/downloads/)  OMG!!!  Xcode !!! YES; take chance and download it!!!
+
+			good luck.......
