8ddfc3849c7a815814bc9889d45d9c774769e456
game <liyl_tao@163.com>
2017-03-30 23:32:41 +0800

    Update install Xcode.js
    change the url to download xcode

--- install Xcode.js
+++ install Xcode.js
@@ -3,6 +3,6 @@ How to install Xcode ?
 2.On webSite ? May be is right
 
 	first：login in Apple Developr webSite  (https://developer.apple.com/)
-	second : go to this page : (https://developer.apple.com/downloads/)  OMG!!!  Xcode !!! YES; take chance and download it!!!
+	second : go to this page : (https://developer.apple.com/download/more/)  OMG!!!  Xcode !!! YES; take chance and download it!!!
 
 			good luck.......
